0d599bce29af1eda1cf88507950617f3f2347daa
klxqz <klxqz@mail.ru>
2014-01-28 00:28:01 +0600

    правки конфига базы

--- lib/config/db.php
+++ lib/config/db.php
@@ -1,6 +1,7 @@
 <?php
+
 return array(
-    'shop_affiliate_transaction' => array(
+    'wastat_developer_products' => array(
         'id' => array('int', 11, 'unsigned' => 1, 'null' => 0, 'autoincrement' => 1),
         'wa_product_id' => array('int', 11, 'unsigned' => 1, 'null' => 0),
         'type' => array('enum', "'apps','plugins','design'", 'null' => 0),
@@ -17,5 +18,19 @@ return array(
             'type' => array('type', 'app', 'code_name', 'unique' => 1),
         ),
     ),
-  
+    'wastat_transaction' => array(
+        'id' => array('int', 11, 'unsigned' => 1, 'null' => 0, 'autoincrement' => 1),
+        'date' => array('datetime', 'null' => 0),
+        'before' => array('decimal', "15,2", 'null' => 0),
+        'amount' => array('decimal', "15,2", 'null' => 0),
+        'after' => array('decimal', "15,2", 'null' => 0),
+        'order_id' => array('int', 11, 'unsigned' => 1, 'null' => 0),
+        'comment' => array('varchar', 255, 'null' => 0),
+        'wa_product_id' => array('int', 11, 'unsigned' => 1, 'null' => 0),
+        ':keys' => array(
+            'PRIMARY' => 'id',
+            'wa_product_id' => 'wa_product_id',
+            'order_id' => 'wa_product_id',
+        ),
+    ),
 );
